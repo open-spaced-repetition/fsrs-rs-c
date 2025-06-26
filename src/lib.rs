c798cc3c4353102061a7d056c10f7fb3df1be164
Asuka Minato <i@asukaminato.eu.org>
2025-06-27 02:22:53 +0900

    #[no_mangle]

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ pub struct FsrsReviews {
 /// # Safety
 ///
 /// The `parameters` pointer must be a valid pointer to an array of f32 with `len` elements.
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn fsrs_new(parameters: *const f32, len: usize) -> *mut FSRS {
     let params = if parameters.is_null() {
         None
@@ -34,7 +34,7 @@ pub unsafe extern "C" fn fsrs_new(parameters: *const f32, len: usize) -> *mut FS
 /// # Safety
 ///
 /// The `fsrs` pointer must be a valid pointer to an FSRS instance created by `fsrs_new`.
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn fsrs_free(fsrs: *mut FSRS) {
     if !fsrs.is_null() {
         drop(Box::from_raw(fsrs));
@@ -47,7 +47,7 @@ pub unsafe extern "C" fn fsrs_free(fsrs: *mut FSRS) {
 ///
 /// The `fsrs` pointer must be a valid pointer to an FSRS instance.
 /// The `memory_state` pointer must be a valid pointer to a MemoryState instance.
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn fsrs_next_states(
     fsrs: *mut FSRS,
     memory_state: *mut MemoryState,
@@ -71,7 +71,7 @@ pub unsafe extern "C" fn fsrs_next_states(
 /// # Safety
 ///
   /// The `next_states` pointer must be a valid pointer to a NextStates instance created by `fsrs_next_states`.
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn fsrs_next_states_free(next_states: *mut NextStates) {
     if !next_states.is_null() {
         drop(Box::from_raw(next_states));
@@ -83,7 +83,7 @@ pub unsafe extern "C" fn fsrs_next_states_free(next_states: *mut NextStates) {
 /// # Safety
 ///
 /// The `next_states` pointer must be a valid pointer to a NextStates instance.
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn fsrs_next_states_again(next_states: *const NextStates) -> ItemState {
     (*next_states).again.clone()
 }
@@ -93,7 +93,7 @@ pub unsafe extern "C" fn fsrs_next_states_again(next_states: *const NextStates) 
 /// # Safety
 ///
 /// The `next_states` pointer must be a valid pointer to a NextStates instance.
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn fsrs_next_states_hard(next_states: *const NextStates) -> ItemState {
     (*next_states).hard.clone()
 }
@@ -103,7 +103,7 @@ pub unsafe extern "C" fn fsrs_next_states_hard(next_states: *const NextStates) -
 /// # Safety
 ///
 /// The `next_states` pointer must be a valid pointer to a NextStates instance.
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn fsrs_next_states_good(next_states: *const NextStates) -> ItemState {
     (*next_states).good.clone()
 }
@@ -113,7 +113,7 @@ pub unsafe extern "C" fn fsrs_next_states_good(next_states: *const NextStates) -
 /// # Safety
 ///
 /// The `next_states` pointer must be a valid pointer to a NextStates instance.
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn fsrs_next_states_easy(next_states: *const NextStates) -> ItemState {
     (*next_states).easy.clone()
 }
@@ -124,7 +124,7 @@ pub unsafe extern "C" fn fsrs_next_states_easy(next_states: *const NextStates) -
 ///
 /// The `fsrs` pointer must be a valid pointer to an FSRS instance.
 /// The `train_set` pointer must be a valid pointer to an array of FSRSItem.
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn fsrs_compute_parameters(
     fsrs: *mut FSRS,
     train_set: *mut FsrsItems,
@@ -147,7 +147,7 @@ pub unsafe extern "C" fn fsrs_compute_parameters(
 /// # Safety
 ///
 /// The `params` pointer must be a valid pointer to the parameters created by `fsrs_compute_parameters`.
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn fsrs_parameters_free(params: *mut f32) {
     if !params.is_null() {
         drop(Box::from_raw(params));
@@ -155,7 +155,7 @@ pub unsafe extern "C" fn fsrs_parameters_free(params: *mut f32) {
 }
 
 /// Creates a new MemoryState instance.
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn fsrs_memory_state_new(stability: f32, difficulty: f32) -> *mut MemoryState {
     Box::into_raw(Box::new(MemoryState {
         stability,
@@ -168,7 +168,7 @@ pub extern "C" fn fsrs_memory_state_new(stability: f32, difficulty: f32) -> *mut
 /// # Safety
 ///
 /// The `memory_state` pointer must be a valid pointer to a MemoryState instance created by `fsrs_memory_state_new`.
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn fsrs_memory_state_free(memory_state: *mut MemoryState) {
     if !memory_state.is_null() {
         drop(Box::from_raw(memory_state));
@@ -180,7 +180,7 @@ pub unsafe extern "C" fn fsrs_memory_state_free(memory_state: *mut MemoryState) 
 /// # Safety
 ///
 /// The `reviews` pointer must be a valid pointer to an array of FSRSReview.
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn fsrs_item_new(reviews: *mut FsrsReviews) -> *mut FSRSItem {
     let reviews = std::slice::from_raw_parts((*reviews).reviews, (*reviews).len).to_vec();
     Box::into_raw(Box::new(FSRSItem { reviews }))
@@ -191,7 +191,7 @@ pub unsafe extern "C" fn fsrs_item_new(reviews: *mut FsrsReviews) -> *mut FSRSIt
 /// # Safety
 ///
 /// The `item` pointer must be a valid pointer to an FSRSItem instance created by `fsrs_item_new`.
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn fsrs_item_free(item: *mut FSRSItem) {
     if !item.is_null() {
         drop(Box::from_raw(item));
@@ -199,7 +199,7 @@ pub unsafe extern "C" fn fsrs_item_free(item: *mut FSRSItem) {
 }
 
 /// Creates a new FSRSReview instance.
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn fsrs_review_new(rating: u32, delta_t: u32) -> *mut FSRSReview {
     Box::into_raw(Box::new(FSRSReview { rating, delta_t }))
 }
@@ -209,7 +209,7 @@ pub extern "C" fn fsrs_review_new(rating: u32, delta_t: u32) -> *mut FSRSReview 
 /// # Safety
 ///
 /// The `review` pointer must be a valid pointer to an FSRSReview instance created by `fsrs_review_new`.
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn fsrs_review_free(review: *mut FSRSReview) {
     if !review.is_null() {
         drop(Box::from_raw(review));
